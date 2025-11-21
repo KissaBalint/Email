6a94f1d0964c924a832f31a3476e122078213bcd
KissBa <kissb1@kkszki.hu>
2025-11-21 11:04:54 +0100

    Menürendszer

--- Email/Program.cs
+++ Email/Program.cs
@@ -10,6 +10,28 @@ namespace Email
     {
         static void Main(string[] args)
         {
+         while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("0 - Kilépés");
+                Console.WriteLine("1 - e-mailok listázása");
+                Console.WriteLine("2 - új e-mail");
+                Console.WriteLine("3 - e-mail törlése");
+                Console.WriteLine("4 - érvényes e-mailok listázása");
+
+                string valasztas = Console.ReadLine();
+
+                switch (valasztas) 
+                {
+                    case "0" : return;
+                    case "1" : Console.WriteLine("e-mailok listázása"); break;
+                    case "2" : Console.WriteLine("új e-mail"); break;
+                    case "3" : Console.WriteLine("e-mail törlése"); break;
+                    case "4" : Console.WriteLine("érvényes e-mailok listázása"); break;
+                    default : Console.WriteLine("rossz parancs");break;
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
